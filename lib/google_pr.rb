a454f2be4aedc59221cc3810519cabc10b176198
Fernando Gomes <feromes@gmail.com>
2008-12-11 14:40:18 -0200

    Add some documentation

--- lib/google_pr.rb
+++ lib/google_pr.rb
@@ -16,6 +16,7 @@ class GooglePR
   M=0x100000000 # modulo for unsigned int 32bit(4byte)
   attr_accessor :uri
 
+  # Create a new GooglePR object with the given 'uri' parameter
   def initialize(uri)
     @uri = uri
   end
@@ -53,7 +54,9 @@ class GooglePR
     a,b,c = mix(a,b,c)
     return c
   end
-
+  
+  # Calculates de checksum to use as 'ch' parameter on request_uri, 
+  # for example the checksum of www.rubyonrails.com is 63157409018
   def cn
     ch = old_cn
     ch = ((ch/7) << 2) | ((ch-(ch/13).floor*13)&7)
@@ -62,11 +65,14 @@ class GooglePR
     ('6' + old_cn(new_url).to_s).to_i
   end
 
+  # URI that gets the pagerank, 
+  # for example to get the Google's pagerank of www.rubyonrails.com the request_uri is http://toolbarqueries.google.com/search?client=navclient-auto&hl=en&ch=63157409018&ie=UTF-8&oe=UTF-8&features=Rank&q=info:http%3A%2F%2Fwww.rubyonrails.com
   def request_uri
     # http://www.bigbold.com/snippets/posts/show/1260 + _ -> %5F
     "http://toolbarqueries.google.com/search?client=navclient-auto&hl=en&ch=#{cn}&ie=UTF-8&oe=UTF-8&features=Rank&q=info:#{URI.escape(@uri, /[^-.!~*'()a-zA-Z\d]/)}"
   end
 
+  # Return a number between 0 to 10, that represents the Google PageRank
   def page_rank(uri = @uri)
     @uri = uri if uri != @uri
     open(request_uri) { |f| return $1.to_i if f.string =~ /Rank_1:\d:(\d+)/ }
